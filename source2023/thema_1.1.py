3d0b122b4c65edda0d7fb91350f6cf5dd70311f7
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-07-02 00:57:18 +0300

    thema_1.1.py updated.

--- source2023/thema_1.1.py
+++ source2023/thema_1.1.py
@@ -1,6 +1,5 @@
-from source2023.videoFunction import *
 from source2023.imageFunction import *
-import numpy as np
+from source2023.videoFunction import *
 
 videoPath = "../auxiliary2023/OriginalVideos/thema_1.avi"
 
@@ -47,28 +46,117 @@ def videoEncoder():
     # Save the video properties to a binary file
     videoSpecs = np.array([len(frames), frames[0].shape[0], frames[0].shape[1], fps], dtype='float64')
 
-    # Huffman encoding
+    print(f'The video has {len(seqErrorImages)} error frames.')
+    frame = seqErrorImages[131]
+    huffmanCompression(frame, frame.size)
+
+
+def huffmanCompression(frame, N):
     # Create a dictionary with the symbols and their probabilities
+    symbols = np.unique(frame, return_counts=True)  # Find the unique symbols and their counts
 
-    symbols = np.unique(seqErrorImages, return_counts=True)  # Find the unique symbols and their counts
-    symbols = np.array(symbols).T  # Transpose the array
-    # symbols = symbols[symbols[:, 1].argsort()[::-1]]  # Sort the symbols by their probabilities
-    symbols = symbols[symbols[:, 0].argsort()]  # Sort the symbols by their values
-    symbols = symbols[symbols[:, 1] != 0]  # Remove the symbols with probability 0
-    # symbols[:, 1] = symbols[:, 1] / np.sum(symbols[:, 1])  # Normalize the probabilities
+    # Transpose the array to have the symbols in the first column and their counts in the second
+    tempSymbols = np.array(symbols).T.tolist()
 
-    # Create the Huffman tree
-    tree = huffmanTree(symbols)
+    # Create a # list of probabilities where the index of each probability corresponds to each symbol
+    probList = [x[1] / N for x in tempSymbols]
 
+    probList = [0.4, 0.3, 0.15, 0.1, 0.05]
+    # probList = [0.3, 0.2, 0.2, 0.1, 0.1, 0.1]
     # Create the Huffman codebook
-    codebook = huffmanCodebook(tree)
+    codebook = huffmanEncode(probList.copy())
+    print(codebook)
 
-    # Encode the error frames
-    encodedErrorFrames = huffmanEncode(seqErrorImages, codebook)
 
-    # Save the encoded error frames to a binary file
-    with open('../auxiliary2023/EncodedVideos/thema_1_1_encodedErrorFrames.pkl', 'wb') as file:
-        pickle.dump(encodedErrorFrames, file)
+def huffmanEncode(probList):
+    # Huffman encoding
+    def findMin():
+        nonlocal foundInStartingProbList_1, foundInStartingProbList_2, whoAmI  # nonlocal is used to access variables
+        # from the outer scope
+        minProb = min(probList)
+        minIdx = probList.index(minProb)
+        if minIdx < size:
+            probList[minIdx] = 2  # Set the minimum probability to 2 so that it is not found again
+            if whoAmI == 1:
+                foundInStartingProbList_1 = True
+            else:  # whoAmI == 2
+                foundInStartingProbList_2 = True
+
+        else:
+            probList.pop(minIdx)
+        return minProb, minIdx
+
+    size = len(probList)  # The number of symbols
+    encodingList = ['' for i in range(len(probList))]
+    sumDict = {}
+
+    # min1 and min2 are the two smallest probabilities (min1 < min2)
+    # min1 -> 0
+    # min2 -> 1
+
+    while probList[-1] != 1:
+        # if len(probList) == size + 1 and probList.count(2) == size:
+        #     break  # End of the algorithm
+
+        # Find the two smallest probabilities
+        foundInStartingProbList_1 = False
+        foundInStartingProbList_2 = False
+        whoAmI = 1
+        minProb_1, minIdx_1 = findMin()
+        whoAmI = 2
+        minProb_2, minIdx_2 = findMin()
+
+        # Fill the encoding list
+        prob_1_found = False
+        prob_2_found = False
+        symbolsOfProb_1 = None
+        symbolsOfProb_2 = None
+
+        for symbols, prob in sumDict.items():
+            if (foundInStartingProbList_1 and foundInStartingProbList_2) or prob_1_found and prob_2_found:
+                break
+            if not foundInStartingProbList_1 and not prob_1_found and minProb_1 == prob:
+                prob_1_found = True
+                symbolsOfProb_1 = symbols  # GH
+                continue
+            elif not foundInStartingProbList_2 and not prob_2_found and minProb_2 == prob:
+                prob_2_found = True
+                symbolsOfProb_2 = symbols
+                continue
+
+        if prob_1_found and prob_2_found:
+            sumDict.pop(symbolsOfProb_1)
+            sumDict.pop(symbolsOfProb_2)
+            sumDict[symbolsOfProb_1 + symbolsOfProb_2] = minProb_1 + minProb_2
+            probList.append(minProb_1 + minProb_2)
+            for symbol in symbolsOfProb_1:
+                encodingList[int(symbol)] += '0'
+            for symbol in symbolsOfProb_2:
+                encodingList[int(symbol)] += '1'
+        elif prob_1_found:
+            sumDict.pop(symbolsOfProb_1)
+            sumDict[symbolsOfProb_1 + str(minIdx_2)] = minProb_1 + minProb_2
+            probList.append(minProb_1 + minProb_2)
+            for symbol in symbolsOfProb_1:
+                encodingList[int(symbol)] += '0'
+            encodingList[minIdx_2] += '1'
+        elif prob_2_found:
+            sumDict.pop(symbolsOfProb_2)
+            sumDict[symbolsOfProb_2 + str(minIdx_1)] = minProb_1 + minProb_2
+            probList.append(minProb_1 + minProb_2)
+            encodingList[minIdx_1] += '0'
+            for symbol in symbolsOfProb_2:
+                encodingList[int(symbol)] += '1'
+        else:
+            sumDict[str(minIdx_1) + str(minIdx_2)] = minProb_1 + minProb_2
+            probList.append(minProb_1 + minProb_2)
+            encodingList[minIdx_1] += '0'
+            encodingList[minIdx_2] += '1'
+    return encodingList
+
+    # # Save the encoded error frames to a binary file
+    # with open('../auxiliary2023/EncodedVideos/thema_1_1_encodedErrorFrames.pkl', 'wb') as file:
+    #     pickle.dump(encodedErrorFrames, file)
 
     # # Calculate the entropy of the error frames sequence
     # H = entropy_score(seqErrorImages)
