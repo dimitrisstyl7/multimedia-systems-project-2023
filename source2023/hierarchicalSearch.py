37bc58c300fcd2b0391b55dc41f9ce1819a29c3d
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-07-04 01:19:44 +0300

    hierarchicalSearch.py updated.

--- source2023/hierarchicalSearch.py
+++ source2023/hierarchicalSearch.py
@@ -8,8 +8,9 @@ def hierarchicalSearch(frames, width, height):
     """
     motion_vectors = []
     for P in range(1, len(frames)):
-        prev_frame = frames[30]
-        curr_frame = frames[31]
+        print("Hierarchical search for frame: ", P)
+        prev_frame = frames[P - 1]
+        curr_frame = frames[P]
 
         # Subsample previous and current frames in 3 levels
         curr_frame_levels = getFrameLevels(curr_frame, width, height)
@@ -17,20 +18,31 @@ def hierarchicalSearch(frames, width, height):
 
         # Execute the hierarchical search algorithm for each level
         levels = len(curr_frame_levels)
+        MV_n_SAD = None
+        temp_width = width
+        temp_height = height
         for level in range(0, levels):
             macroblock_size = getMacroblockSize(level)
-            k = getSearchRadius(level)  # search radius (in pixels)
-            width, height = getWidthAndHeight(level, width, height)
-            executeLevel(curr_frame_levels[level], prev_frame_levels[level], macroblock_size, width, height)
+            temp_width, temp_height = getWidthAndHeight(level, temp_width, temp_height)
+            MV_n_SAD = executeLevel(MV_n_SAD, curr_frame_levels[level], prev_frame_levels[level], macroblock_size,
+                                    temp_width, temp_height)
+        motion_vectors.append([(x, y) for (x, y), SAD in MV_n_SAD])
+    return motion_vectors
 
 
 def getFrameLevels(frame, width, height):
+    """
+        Subsample the frame in 3 levels.
+    """
     return [frame,
             cv2.resize(frame, (width // 2, height // 2), interpolation=cv2.INTER_LINEAR),
             cv2.resize(frame, (width // 4, height // 4), interpolation=cv2.INTER_LINEAR)]
 
 
 def getMacroblockSize(level):
+    """
+        Get the macroblock size for each level.
+    """
     if level == 0:
         return 64
     elif level == 1:
@@ -38,23 +50,21 @@ def getMacroblockSize(level):
     return 16  # level 2
 
 
-def getSearchRadius(level):
-    if level == 0:
-        return 32
-    elif level == 1:
-        return 16
-    return 8  # level 2
-
-
 def getWidthAndHeight(level, width, height):
+    """
+        Get the width and height for each level.
+    """
     if level == 0:
         return width, height
-    elif level == 1:
-        return width // 2, height // 2
-    return width // 4, height // 4  # level 2
+    return width // 2, height // 2  # level 1 and 2
 
 
-def executeLevel(curr_frame, prev_frame, macroblock_size, width, height):
+def executeLevel(MV_n_SAD, curr_frame, prev_frame, macroblock_size, width, height):
+    """
+    Execute the hierarchical search algorithm for each level. Because search radius (k) is always smaller than the
+    macroblock size, the search window is always to the nearest neighbors of the current macroblock. So our
+    implementation practically doesn't need the variable k.
+    """
     no_of_cols = width // macroblock_size  # number of macroblocks per row
     no_of_rows = height // macroblock_size  # number of macroblocks per column
     curr_frame_macroblocks = divideFrameIntoMacroblocks(curr_frame, macroblock_size, width, height, no_of_rows,
@@ -62,14 +72,22 @@ def executeLevel(curr_frame, prev_frame, macroblock_size, width, height):
     prev_frame_macroblocks = divideFrameIntoMacroblocks(prev_frame, macroblock_size, width, height, no_of_rows,
                                                         no_of_cols)
     matched_macroblocks = getSADErrorValues(curr_frame_macroblocks, prev_frame_macroblocks, no_of_rows, no_of_cols)
-    MV_n_SAD = calculateMotionVectors(matched_macroblocks, macroblock_size, no_of_cols)
+
+    if MV_n_SAD is None:  # first level
+        return calculateMotionVectors(matched_macroblocks, macroblock_size, no_of_cols)  # return the motion vectors and
+    # SAD values
+
+    # second and third level
+    MV_n_SAD = [[(x // 2, y // 2), SAD] for (x, y), SAD in MV_n_SAD]  # divide the motion vectors by 2
+    MV_n_SAD_new = calculateMotionVectors(matched_macroblocks, macroblock_size, no_of_cols)
+    return compareMV_n_SAD(MV_n_SAD, MV_n_SAD_new)  # return the updated motion vectors and SAD values, if needed
 
 
 def divideFrameIntoMacroblocks(frame, macroblock_size, width, height, no_of_rows, no_of_cols):
     """
         Divide the frame into macroblocks.
     """
-    macroblocks = [[0 for i in range(no_of_cols)] for j in range(no_of_rows)]  # 2D array
+    macroblocks = [[0 for _ in range(no_of_cols)] for _ in range(no_of_rows)]  # 2D array
     i, j = 0, 0
 
     for row in range(0, height, macroblock_size):
@@ -90,8 +108,8 @@ def getSADErrorValues(curr_frame_macroblocks, prev_frame_macroblocks, no_of_rows
     matched_macroblocks = []  # Matched macroblocks, contains lists in the form: [(i, j), SAD_value] where (i, j)
     # is the coordinate of the matched macroblock in the previous frame
 
-    for i in range(len(curr_frame_macroblocks)):
-        for j in range(len(curr_frame_macroblocks[i])):
+    for i in range(no_of_rows):
+        for j in range(no_of_cols):
             neigh_prev_macroblocks_coord = [(i, j)]  # Contains the coordinates of the previous macroblocks that are
             # neighbors of the current macroblock
             neigh_prev_SAD_values = \
@@ -162,7 +180,6 @@ def calculateMotionVectors(matched_macroblocks, macroblock_size, no_of_cols):
         Calculate the motion vectors for each matched macroblock.
     """
     MV_n_SAD = []  # Contains lists in the form: [motion_vector, SAD_value]
-    dx, dy = 0, 0
     for i in range(len(matched_macroblocks)):
         # Find the coordinates of the current macroblock
         curr_macroblock_row = i // no_of_cols
@@ -178,7 +195,17 @@ def calculateMotionVectors(matched_macroblocks, macroblock_size, no_of_cols):
         prev_pixel = (prev_macroblock_col * macroblock_size, prev_macroblock_row * macroblock_size)
 
         # Calculate the motion vector, form: (dx, dy)
-        motion_vector = (dx + curr_pixel[0] - prev_pixel[0], dy + curr_pixel[1] - prev_pixel[1])
+        motion_vector = (curr_pixel[0] - prev_pixel[0], curr_pixel[1] - prev_pixel[1])
         SAD_value = matched_macroblocks[i][1]  # SAD value of the matched macroblock
         MV_n_SAD.append([motion_vector, SAD_value])  # Append the motion vector and the SAD value to the list
+    return MV_n_SAD
+
+
+def compareMV_n_SAD(MV_n_SAD, MV_n_SAD_new):
+    """
+        Compare and update the motion vectors and SAD values, if necessary.
+    """
+    for i in range(len(MV_n_SAD)):
+        if MV_n_SAD[i][1] > MV_n_SAD_new[i][1]:
+            MV_n_SAD[i] = MV_n_SAD_new[i]
     return MV_n_SAD
