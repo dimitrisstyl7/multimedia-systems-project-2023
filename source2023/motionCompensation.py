825c4ba35320d4df00bcc9cf53f973e527b78a5d
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-08-27 18:15:14 +0300

    motionCompensation.py updated.

--- source2023/motionCompensation.py
+++ source2023/motionCompensation.py
@@ -11,6 +11,7 @@ def motionCompensationForEncoding(frames, motionVectors, width):
     motionCompensatedFrames = [frames[0]]  # I frame
 
     for i in range(1, len(frames)):
+        print(f'\nFrame {i} of {len(frames) - 1}')
         # Get the reference (previous) and target (current) frame
         referenceFrame = frames[i - 1]
         targetFrame = np.zeros_like(referenceFrame)
@@ -18,15 +19,15 @@ def motionCompensationForEncoding(frames, motionVectors, width):
         noOfMacroblocks = len(motionVectors[idxOfVectorsForCurrFrame])
 
         for j in range(noOfMacroblocks):
+            print(f'\tMacroblock {j} of {noOfMacroblocks - 1}')
             # Get the motion vector of the current macroblock
             motionVector = motionVectors[idxOfVectorsForCurrFrame][j][0]
 
             # Get the starting pixel of the current macroblock
-            macroblockIdx = (j % noOfCols, j // noOfCols)  # (x, y)
-            startingRefPixel = (macroblockIdx[0] * macroblockSize, macroblockIdx[1] * macroblockSize)  # (x, y)
+            macroblockIdx = (j // noOfCols, j % noOfCols)  # (y, x)
+            startingRefPixel = (macroblockIdx[0] * macroblockSize, macroblockIdx[1] * macroblockSize)  # (y, x)
             targetFrame = motionCompensationForEncodingOnSpecificFrame(motionVector, targetFrame, referenceFrame,
                                                                        startingRefPixel)
-
         motionCompensatedFrames.append(targetFrame)
     return motionCompensatedFrames
 
@@ -40,8 +41,8 @@ def motionCompensationForDecoding(iFrame, motionVectors):
         referenceFrame = motionCompensatedFrames[-1]  # Reference frame is the last frame in the list
         targetFrame = referenceFrame.copy()
         for j in range(len(motionVectors[i])):  # len(motionVectors[i]) equals to the number of macroblocks in the frame
-            startingPixel = (j // macroblockSize, j % macroblockSize)  # (x, y)
-            motionVector = motionVectors[i][j]  # Get the motion vector of the current macroblock, (x, y)
+            startingPixel = (j % macroblockSize, j // macroblockSize)  # (y, x)
+            motionVector = motionVectors[i][j]  # Get the motion vector of the current macroblock, (y, x)
         motionCompensatedFrames.append(
             motionCompensationForSpecificFrame(motionVector, targetFrame, referenceFrame, startingPixel))
     return motionCompensatedFrames
@@ -51,87 +52,45 @@ def motionCompensationForEncodingOnSpecificFrame(motionVector, targetFrame, refe
     """
         Perform motion compensation on a specific frame.
     """
-    '''if motionVector[0] < 0:  # x < 0
-        if motionVector[1] < 0:  # y < 0
+    if motionVector[1] < 0:  # x < 0
+        if motionVector[0] < 0:  # y < 0
             # Move left and up
-            return performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
 
-        if motionVector[1] > 0:  # y > 0
+        if motionVector[0] > 0:  # y > 0
             # Move left and down
-            targetFrame[
-            startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-            startingRefPixel[0]:startingRefPixel[0] + macroblockSize
-            ] = referenceFrame[
-                startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-                startingRefPixel[0]:startingRefPixel[0] + macroblockSize
-                ]
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
         else:  # y == 0
             # Move left
-            targetFrame[
-            startingRefPixel[1],
-            startingRefPixel[0]:startingRefPixel[0] + macroblockSize
-            ] = referenceFrame[
-                startingRefPixel[1],
-                startingRefPixel[0]:startingRefPixel[0] + macroblockSize
-                ]
-    elif motionVector[0] > 0:  # x > 0
-        if motionVector[1] < 0:  # y < 0
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
+    elif motionVector[1] > 0:  # x > 0
+        if motionVector[0] < 0:  # y < 0
             # Move right and up
-            targetFrame[
-            startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-            startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-            ] = referenceFrame[
-                startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-                startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-                ]
-        if motionVector[1] > 0:  # y > 0
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
+        if motionVector[0] > 0:  # y > 0
             # Move right and down
-            targetFrame[
-            startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-            startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-            ] = referenceFrame[
-                startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-                startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-                ]
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
         else:  # y == 0
             # Move right
-            targetFrame[
-            startingRefPixel[1],
-            startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-            ] = referenceFrame[
-                startingRefPixel[1],
-                startingRefPixel[0]:startingRefPixel[0] - macroblockSize
-                ]
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
     else:  # x == 0
-        if motionVector[1] < 0:  # y < 0
+        if motionVector[0] < 0:  # y < 0
             # Move up
-            targetFrame[
-            startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-            startingRefPixel[0]
-            ] = referenceFrame[
-                startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-                startingRefPixel[0]
-                ]
-        if motionVector[1] > 0:  # y > 0
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
+        if motionVector[0] > 0:  # y > 0
             # Move down
-            targetFrame[
-            startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-            startingRefPixel[0]
-            ] = referenceFrame[
-                startingRefPixel[1]:startingRefPixel[1] - macroblockSize,
-                startingRefPixel[0]
-                ]
+            performMotionCompensation(startingRefPixel, motionVector, referenceFrame, targetFrame)
+
         else:  # y == 0
             # No movement
-            targetFrame = referenceFrame'''
-    startingTargetPixel = (startingRefPixel[0] + motionVector[0], startingRefPixel[1] + motionVector[1])
-    targetFrame[
-    startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize,
-    startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize
-    ] = referenceFrame[
-        startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-        startingRefPixel[0]:startingRefPixel[0] + macroblockSize
-        ]
+            targetFrame = referenceFrame
+
     return targetFrame
 
 
@@ -143,44 +102,74 @@ def performMotionCompensation(startingRefPixel, motionVector, referenceFrame, ta
 
     if startingTargetPixel[0] < 0 and startingTargetPixel[1] < 0:
         # out of bounds (top left)
-        return True
+        ''' CODE HERE '''
+        return targetFrame
 
     if startingTargetPixel[0] + macroblockSize > targetFrame.shape[0] and \
             startingTargetPixel[1] + macroblockSize > targetFrame.shape[1]:
         # out of bounds (bottom right)
-        return True
-
-    if startingTargetPixel[0] + macroblockSize > targetFrame.shape[0] and startingTargetPixel[1] < 0:
-        # out of bounds (top right)
-        return True
+        targetFrame[
+        startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize,
+        startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize
+        ] = referenceFrame[
+            startingRefPixel[0]:startingRefPixel[0] + macroblockSize - motionVector[0],
+            startingRefPixel[1]:startingRefPixel[1] + macroblockSize - motionVector[1]
+            ]
+        return targetFrame
 
     if startingTargetPixel[0] < 0 and startingTargetPixel[1] + macroblockSize > targetFrame.shape[1]:
-        # out of bounds (bottom left)
-        return True
+        # out of bounds (top right)
+        targetFrame[
+        startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize,
+        startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize
+        ] = referenceFrame[
+            :startingRefPixel[0] + macroblockSize + motionVector[0],
+            ]
+        return targetFrame
 
-    if startingTargetPixel[0] < 0:
-        # out of bounds (left)
-        return True
+    if startingTargetPixel[0] + macroblockSize > targetFrame.shape[0] and startingTargetPixel[1] < 0:
+        # out of bounds (bottom left)
+        ''' CODE HERE '''
+        return targetFrame
 
     if startingTargetPixel[1] < 0:
-        # out of bounds (top)
-        return True
+        # out of bounds (left)
+        ''' CODE HERE '''
+        return targetFrame
 
-    if startingTargetPixel[0] + macroblockSize > targetFrame.shape[0]:
-        # out of bounds (right)
-        return True
+    if startingTargetPixel[0] < 0:
+        # out of bounds (top)
+        ''' CODE HERE '''
+        return targetFrame
 
     if startingTargetPixel[1] + macroblockSize > targetFrame.shape[1]:
+        # out of bounds (right)
+        targetFrame[
+        startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize,
+        startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize
+        ] = referenceFrame[
+            startingRefPixel[0]:startingRefPixel[0] + macroblockSize,
+            startingRefPixel[1]:startingRefPixel[1] + motionVector[1]
+            ]
+        return targetFrame
+
+    if startingTargetPixel[0] + macroblockSize > targetFrame.shape[0]:
         # out of bounds (bottom)
-        return True
+        targetFrame[
+        startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize,
+        startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize
+        ] = referenceFrame[
+            startingRefPixel[0]:startingRefPixel[0] + motionVector[0],
+            startingRefPixel[1]:startingRefPixel[1] + macroblockSize
+            ]
+        return targetFrame
 
     # if no out of bounds
     targetFrame[
-    startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize,
-    startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize
+    startingTargetPixel[0]:startingTargetPixel[0] + macroblockSize,
+    startingTargetPixel[1]:startingTargetPixel[1] + macroblockSize
     ] = referenceFrame[
-        startingRefPixel[1]:startingRefPixel[1] + macroblockSize,
-        startingRefPixel[0]:startingRefPixel[0] + macroblockSize
+        startingRefPixel[0]:startingRefPixel[0] + macroblockSize,
+        startingRefPixel[1]:startingRefPixel[1] + macroblockSize
         ]
-
     return targetFrame
