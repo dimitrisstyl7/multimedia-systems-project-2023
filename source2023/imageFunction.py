7717fd50c2c1a68c51e670470d4367bd08e6ced6
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-09-07 22:44:08 +0300

    imageFunction.py updated.

--- source2023/imageFunction.py
+++ source2023/imageFunction.py
@@ -1,13 +1,4 @@
 import numpy as np
-from PIL import Image
-
-
-def saveImage(image, name):
-    """
-        Save the image
-    """
-    image = Image.fromarray(image.astype(np.uint8))
-    image.save('../auxiliary2023/dump/' + name)
 
 
 def calculateErrorImage(image1, image2):
@@ -15,10 +6,3 @@ def calculateErrorImage(image1, image2):
         Calculate the error image of the current frame
     """
     return np.subtract(image1, image2)
-
-
-def convertToUint8(image):
-    """
-        Convert the image to uint8
-    """
-    return image.astype(np.uint8)
